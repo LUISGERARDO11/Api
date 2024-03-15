d9c597dd91311a1ba218eabc916f9a17d4b9881f
LUISGERARDO11 <luis34julieta@gmail.com>
2024-03-15 00:01:49 -0600

    Modificacion de la ruta para Politicas, añadir el campo de imagen

--- src/models/Politicas.js
+++ src/models/Politicas.js
@@ -4,6 +4,7 @@ const politicachema = mongoose.Schema({
   titulo: { type: String, required: true },
   descripcion: { type: String, required: true },
   fecha_actualizacion: {type: Date, default: Date.now , required: true },
+  image: {type: String, required: false },
 });
 
 module.exports = mongoose.model('Politica', politicachema);
