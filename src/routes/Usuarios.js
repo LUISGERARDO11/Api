75d4c6a07dc31c00e4da8499a82fa1c6eb19991d
LUISGERARDO11 <luis34julieta@gmail.com>
2024-03-18 16:57:18 -0600

    Modificacion de Usuarios para la seguridad

--- src/routes/Usuarios.js
+++ src/routes/Usuarios.js
@@ -1,119 +1,116 @@
-const express=require('express')
-const esquema=require('../models/Usuarios')
+const express = require('express');
+const bcrypt = require('bcrypt'); // Librería para encriptar contraseñas
+const esquema = require('../models/Usuarios');
 
-const router=express.Router()
+const router = express.Router();
 
-//crear un usuario
-router.post('/usuarios',(req,res)=>{
-    const us= esquema(req.body);
-    us.save()
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
-
-//leer usuarios
-router.get('/usuarios',(req,res)=>{
-    esquema.find()
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
-
-//buscar usuario
-router.get('/usuarios/:id',(req,res)=>{
-    const {id}=req.params
-    esquema.findById(id)
-    .then(data=>res.json(data))
-    .catch(error=>res.json({message:error}))
-})
-
-
-router.get('/usuarios/email/:correo', (req, res) => {
-    const { correo } = req.params;
-    esquema.findOne({ correo })
-      .then(data => {
-        // Verifica si se encontró un usuario con el correo proporcionado
-        const exists = data !== null;
-        res.json({ exists, data }); // Devuelve un objeto con la propiedad "exists" y los datos del usuario
-      })
-      .catch(error => res.status(500).json({ message: error }));
+// Crear un usuario
+router.post('/usuarios', async (req, res) => {
+  try {
+    const { contrasenia, ...userData } = req.body; // Excluir la contraseña del objeto userData
+    const hashedPassword = await bcrypt.hash(contrasenia, 10); // Encriptar la contraseña
+    const usuario = new esquema({ ...userData, contrasenia: hashedPassword }); // Usar la contraseña encriptada
+    const savedUser = await usuario.save();
+    res.json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message }); // Manejo específico de errores
+  }
 });
 
-// Método para verificar si hay un documento con el correo, pregunta secreta y respuesta secreta
-router.post('/usuarios/verify', (req, res) => {
-    const { correo, pregunta_secreta, respuesta_secreta } = req.body;
-
-    esquema.findOne({ correo, pregunta_secreta, respuesta_secreta })
-        .then(usuario => {
-            if (usuario) {
-                res.json({ exists: true });
-            } else {
-                res.json({ exists: false });
-            }
-        })
-        .catch(error => res.status(500).json({ message: 'Error al buscar usuario', error }));
+// Leer usuarios
+router.get('/usuarios', async (req, res) => {
+  try {
+    const users = await esquema.find().select('-contrasenia -pregunta_secreta -respuesta_secreta'); // Excluir campos sensibles
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message }); // Manejo específico de errores
+  }
 });
 
-  
-//Login
-  router.post('/usuarios/login', (req, res) => {
-    const { correo, contrasenia } = req.body;
-  
-    esquema.findOne({ correo, contrasenia })
-      .then(usuario => {
-        if (usuario) {
-          res.json({ message: 'Inicio de sesión exitoso', usuario });
-        } else {
-          res.status(404).json({ message: 'Correo electrónico o contraseña incorrectos' });
-        }
-      })
-      .catch(error => res.status(500).json({ message: 'Error al buscar usuario', error }));
+// Buscar usuario por ID
+router.get('/usuarios/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await esquema.findById(id).select('-contrasenia -pregunta_secreta -respuesta_secreta'); // Excluir campos sensibles
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message }); // Manejo específico de errores
+  }
+});
+
+router.get('/usuarios/email/:correo', async (req, res) => {
+    try {
+      const { correo } = req.params;
+      const usuario = await esquema.findOne({ correo }).select('-contrasenia -pregunta_secreta -respuesta_secreta'); // Excluir campos sensibles
+      const exists = usuario !== null;
+      res.json({ exists, usuario }); // Devuelve un objeto con la propiedad "exists" y los datos del usuario sin información sensible
+    } catch (error) {
+      res.status(500).json({ message: 'Error al buscar usuario', error }); // Manejo específico de errores
+    }
   });
 
-  // Eliminar usuario por ID
-router.delete('/usuarios/:id', (req, res) => {
+// Método para verificar si existe un usuario con el correo, pregunta secreta y respuesta secreta
+router.post('/usuarios/verify', async (req, res) => {
+  try {
+    const { correo, pregunta_secreta, respuesta_secreta } = req.body;
+    const usuario = await esquema.findOne({ correo });
+
+    if (usuario && await bcrypt.compare(respuesta_secreta, usuario.respuesta_secreta)) {
+      res.json({ exists: true });
+    } else {
+      res.json({ exists: false });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al buscar usuario', error }); // Manejo específico de errores
+  }
+});
+
+// Inicio de sesión
+router.post('/usuarios/login', async (req, res) => {
+  try {
+    const { correo, contrasenia } = req.body;
+    const usuario = await esquema.findOne({ correo });
+
+    if (usuario && await bcrypt.compare(contrasenia, usuario.contrasenia)) {
+      res.json({ message: 'Inicio de sesión exitoso', usuario });
+    } else {
+      res.status(404).json({ message: 'Correo electrónico o contraseña incorrectos' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Error al buscar usuario', error }); // Manejo específico de errores
+  }
+});
+
+// Eliminar usuario por ID
+router.delete('/usuarios/:id', async (req, res) => {
+  try {
     const { id } = req.params;
-
-    esquema.findByIdAndDelete(id)
-        .then(data => {
-            if (data) {
-                res.json({ message: 'Usuario eliminado correctamente', usuarioEliminado: data });
-            } else {
-                res.status(404).json({ message: 'No se encontró ningún usuario con el ID proporcionado' });
-            }
-        })
-        .catch(error => res.status(500).json({ message: 'Error al eliminar usuario', error }));
+    const deletedUser = await esquema.findByIdAndDelete(id);
+    res.json({ message: 'Usuario eliminado correctamente', usuarioEliminado: deletedUser });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar usuario', error }); // Manejo específico de errores
+  }
 });
+
 // Actualizar todos los campos de la dirección
-router.put('/usuarios/direccion/:id', (req, res) => {
-  const { id } = req.params;
-  const newDireccion = req.body;
-
-  esquema.findByIdAndUpdate(id, { direccion: newDireccion }, { new: true })
-      .then(updatedUser => {
-          if (updatedUser) {
-              res.json({ message: 'Dirección actualizada correctamente', usuarioActualizado: updatedUser });
-          } else {
-              res.status(404).json({ message: 'No se encontró ningún usuario con el ID proporcionado' });
-          }
-      })
-      .catch(error => res.status(500).json({ message: 'Error al actualizar dirección del usuario', error }));
+router.put('/usuarios/direccion/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const newDireccion = req.body;
+    const updatedUser = await esquema.findByIdAndUpdate(id, { $set: { direccion: newDireccion } }, { new: true }); // Actualizar solo la propiedad "direccion"
+    res.json({ message: 'Dirección actualizada correctamente', usuarioActualizado: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al actualizar dirección del usuario', error }); // Manejo específico de errores
+  }
 });
 
-// Actualizar nombre, teléfono y correo
-router.put('/usuarios/datos/:id', (req, res) => {
-  const { id } = req.params;
-  const { nombre_completo, telefono, correo } = req.body;
-
-  esquema.findByIdAndUpdate(id, { nombre_completo, telefono, correo }, { new: true })
-      .then(updatedUser => {
-          if (updatedUser) {
-              res.json({ message: 'Nombre, teléfono y correo actualizados correctamente', usuarioActualizado: updatedUser });
-          } else {
-              res.status(404).json({ message: 'No se encontró ningún usuario con el ID proporcionado' });
-          }
-      })
-      .catch(error => res.status(500).json({ message: 'Error al actualizar nombre, teléfono y correo del usuario', error }));
-});
-
-
-module.exports=router
+router.put('/usuarios/datos/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { nombre_completo, telefono, correo } = req.body;
+      const updatedUser = await esquema.findByIdAndUpdate(id, { $set: { nombre_completo, telefono, correo } }, { new: true });
+      res.json({ message: 'Nombre, teléfono y correo actualizados correctamente', usuarioActualizado: updatedUser });
+    } catch (error) {
+      res.status(500).json({ message: 'Error al actualizar nombre, teléfono y correo del usuario', error }); // Manejo específico de errores
+    }
+  });
