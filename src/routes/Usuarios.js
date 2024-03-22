c7a1d4a47a33281387f9abff23e3cfd1dc1e7bee
LUISGERARDO11 <luis34julieta@gmail.com>
2024-03-21 22:40:35 -0600

    Añadimos el metodo para buscar a un usuario por nombre O correo x2

--- src/routes/Usuarios.js
+++ src/routes/Usuarios.js
@@ -247,14 +247,15 @@ router.post('/usuarios/eliminardispositivo', async (req, res) => {
     }
 });
 // Buscar usuario por nombre o correo electrónico
-router.get('/usuarios/buscar', async (req, res) => {
-    const { query } = req.query;
+// Buscar usuario por nombre o correo electrónico
+router.get('/usuarios/buscar/:query', async (req, res) => {
+    const { query } = req.params;
 
     try {
         // Buscar usuarios que coincidan con el nombre o correo electrónico
         const usuarios = await esquema.find({
             $or: [
-                { nombre: { $regex: query, $options: 'i' } }, // Búsqueda por nombre (insensible a mayúsculas y minúsculas)
+                { nombre_completo: { $regex: query, $options: 'i' } }, // Búsqueda por nombre (insensible a mayúsculas y minúsculas)
                 { correo: { $regex: query, $options: 'i' } } // Búsqueda por correo electrónico (insensible a mayúsculas y minúsculas)
             ]
         });
@@ -264,5 +265,6 @@ router.get('/usuarios/buscar', async (req, res) => {
         res.status(500).json({ message: 'Error al buscar usuarios', error: error.message });
     }
 });
+
   
 module.exports=router
