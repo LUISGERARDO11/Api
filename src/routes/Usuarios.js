a2b1ee54e15f1b610f223f9800cf9712a4fc6c14
LUISGERARDO11 <luis34julieta@gmail.com>
2024-03-25 17:26:00 -0600

    Prueba para el segundo login pa´ la pagina web

--- src/routes/Usuarios.js
+++ src/routes/Usuarios.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const bcrypt = require('bcrypt');
 const esquema=require('../models/Usuarios')
+const jwt = require('jsonwebtoken'); // Importa el módulo jwt para trabajar con tokens
+const authMiddleware = require('../middlewares/authMiddleware'); // Middleware para validar el token
 
 const router=express.Router()
 
@@ -288,6 +290,26 @@ router.get('/us/usuarioscondispositivos', async (req, res) => {
         res.json(usuariosConDispositivosInfo);
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener usuarios con dispositivos', error: error.message });
+    }
+});
+
+// Endpoint para obtener los datos del usuario actual basados en el token
+router.get('/usuariosme/me', authMiddleware, async (req, res) => {
+    try {
+        // Extraer el ID del usuario del token decodificado
+        const usuarioId = req.usuarioId;
+
+        // Buscar el usuario por ID
+        const usuario = await esquema.findById(usuarioId).select('-contrasenia'); // No enviar la contraseña en la respuesta
+
+        // Verificar si se encontró un usuario con el ID proporcionado
+        if (usuario) {
+            res.json(usuario);
+        } else {
+            res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener los datos del usuario', error: error.message });
     }
 });
 
