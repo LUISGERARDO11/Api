8edaef79a1f48246c0eaa7e27acc9fe87ca5aea5
LUISGERARDO11 <luis34julieta@gmail.com>
2024-04-07 12:49:15 -0600

    cambio del modelo de dispositivos para admitir temperatura y humedad

--- src/models/Dispositivos.js
+++ src/models/Dispositivos.js
@@ -18,6 +18,14 @@ const dispositivoSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Usuario'
   },
+  temperatura: {
+    type: Number, // Tipo de dato para la temperatura (podría ser otro dependiendo de tus necesidades)
+    default: null // Valor por defecto si no se proporciona la temperatura
+  },
+  humedad: {
+    type: Number, // Tipo de dato para la humedad (podría ser otro dependiendo de tus necesidades)
+    default: null // Valor por defecto si no se proporciona la humedad
+  },
   fecha_actualizacion: {
     type: Date,
     default: Date.now 
